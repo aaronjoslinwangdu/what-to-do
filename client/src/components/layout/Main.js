f45b51da99a5d1182e6370a38a54f3c167fdc636
Aaron Joslin-Wangdu <aaronjoslinwangdu@gmail.com>
2023-04-20 17:59:04 -0400

    Add crud functions for items in client and test function in main

--- client/src/components/layout/Main.js
+++ client/src/components/layout/Main.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getItems, getItem, updateItem, deleteItem } from '../../utils/Api';
 
 // Styles
 import styles from '../../assets/css/layout/Main.module.css';
@@ -6,8 +7,26 @@ import styles from '../../assets/css/layout/Main.module.css';
 // Components
 import Item from '../items/Item';
 
+const DUMMY_ITEM = {
+  _id: '6441b25cb5f8079b70f34e1f',
+  label: 'dummy',
+  description: 'dummy desc',
+  date: '04-20-2023',
+  userId: 1,
+  folder: 2,
+  status: 2
+}
 
 const Main = () => {
+
+  useEffect(() => {
+    const testFn = async () => {
+      const itemList = await deleteItem(DUMMY_ITEM._id);
+      console.log(itemList);
+    }
+    testFn();
+  }, []);
+
   return (
     <div className={styles.main}>
       <Item />
--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -1,5 +1,17 @@
 import axios from 'axios';
 
+
+export const getItems = async () => {
+  try {
+    const response = await axios.get('http://localhost:3001/api/item/');
+    const items = response.data;
+    return items;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+
 export const getItem = async (id) => {
   try {
     const response = await axios.get(`http://localhost:3001/api/item/${id}`);
@@ -14,15 +26,8 @@ export const getItem = async (id) => {
 export const addItem = async (item) => {
   try {
     const response = await axios.post(
-      'http://localhost:3001/api/item',
-      {
-        label: item.label,
-        description: item.description,
-        date: item.date,
-        userId: 1,
-        status: item.status,
-        folder: item.folder
-      }
+      'http://localhost:3001/api/item/',
+      item,
     );
     const addedItem = response.data;
     return addedItem;
@@ -31,3 +36,30 @@ export const addItem = async (item) => {
   }
 } 
 
+
+export const updateItem = async (item) => {
+
+  const id = item._id;
+
+  try {
+    const response = await axios.put(
+      `http://localhost:3001/api/item/${id}`,
+      item,
+    );
+    const updatedItem = response.data;
+    return updatedItem;
+  } catch (error) {
+    throw new Error(error);
+  }
+} 
+
+
+export const deleteItem = async (id) => {
+  try {
+    const response = await axios.delete(`http://localhost:3001/api/item/${id}`);
+    const deletedId = response.data.id;
+    return (`Deleted ${deletedId}`);
+  } catch (error) {
+    throw new Error(error);
+  }
+}
