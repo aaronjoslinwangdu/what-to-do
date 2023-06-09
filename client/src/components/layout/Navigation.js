4ca1e6d5e2aaa665203e63e365490f998cf6b816
Aaron Joslin-Wangdu <aaronjoslinwangdu@gmail.com>
2023-06-09 16:57:02 -0400

    Add 'Welcome' message in nav bar and make username clickable to open edit profile form

--- client/src/components/layout/Navigation.js
+++ client/src/components/layout/Navigation.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
+// Styles
 import styles from '../../assets/css/layout/Navigation.module.css';
 
+// Components
+import { layoutActions } from '../../store/layout/layoutSlice';
+
 const NavigationTop = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.auth.user);
+
+  const editProfileHandler = () => {
+    dispatch(layoutActions.setShowProfileForm(true));
+  }
+
   return (
     <nav className={styles.navigation}>
-      <div>Hello</div>
+      <div className={styles.navigationProfile}>
+        <div className={styles.headerText}>
+          <div>Welcome,</div>
+          <div 
+            className={styles.headerMainText}
+            onClick={editProfileHandler}
+          >
+            {user.username}
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
